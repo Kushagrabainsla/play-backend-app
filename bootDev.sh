9f3fe5eb303906de97cdee977e1617f50f42df77
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-09-07 19:16:30 +0530

    eventlet issue

--- bootDev.sh
+++ bootDev.sh
@@ -1,4 +1,5 @@
 export FLASK_APP=play_backend_app
 export FLASK_ENV=development
 flask run
-# gunicorn -k eventlet -w 1 play_backend_app:app
+# gunicorn --worker-class eventlet -w 1 play_backend_app:app
+
