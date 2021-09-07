6d25c30571ca41ce9d9f42fb1d0be03789f8abb3
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-09-07 19:48:42 +0530

    Heroku issue

--- bootDev.sh
+++ bootDev.sh
@@ -1,5 +1,5 @@
-export FLASK_APP=play_backend_app
-export FLASK_ENV=development
-flask run
-# gunicorn --worker-class eventlet -w 1 play_backend_app:app
+# export FLASK_APP=play_backend_app
+# export FLASK_ENV=development
+# flask run
+gunicorn --worker-class eventlet -w 1 play_backend_app:app
 
