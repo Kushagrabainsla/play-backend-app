144acb858bcbc808befabb39986b88b1bff016c3
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-09-13 18:08:21 +0530

    added public info route

--- play_backend_app/views/home.py
+++ play_backend_app/views/home.py
@@ -1,6 +1,22 @@
 from play_backend_app import app
-from flask import render_template
+from flask import render_template, jsonify
 
 @app.route('/')
 def home():
 	return render_template('home.html')
+
+@app.route('/info')
+def appInfo():
+	return jsonify({
+		'error': False,
+  		'allSystemsWorkingFine': True,
+  		'version': '0.0.2',
+		'name': 'play-backend-app',
+  		'author': 'Kushagra Bainsla',
+		'Description': 'RESTful API endpoints and backend service for play.',
+		'routes': {
+			'public': 3,
+			'protected': 8,
+      	},
+		'sockets': 2,
+	})
