2703e9f831851d7a579296d337fac22aec6ac9f5
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-10-17 16:51:59 +0530

    File uploading scripts initiated

--- play_backend_app/routes/user.py
+++ play_backend_app/routes/user.py
@@ -1,7 +1,8 @@
 import os
 from play_backend_app import app
 from .. import db
-from flask import request, jsonify
+from flask import request, jsonify, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 
 # ( GET REQUEST ) For getting user's profile details.
 @app.route('/user/profile')
@@ -71,4 +72,49 @@ def allConnections():
         return jsonify({
             'error': True,
             'message' : 'Invalid request method.' 
+        })
+
+
+
+
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/uploads/<filename>')
+def downloadFile(filename):
+    return send_from_directory(os.path.join(app.root_path, 'uploads'), filename=filename)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def uploadfile():
+    if request.method == 'POST':
+        print(request.headers)
+        file = request.files.get('file')
+        if not file or file.filename == '': 
+            return jsonify({
+                'error': True,
+                'message': 'File not present.'
+            })
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            try:
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            except:
+                os.mkdir(app.config['UPLOAD_FOLDER'])
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        fileUrl = url_for('downloadFile', filename=filename)
+        # Update the url for the saved image in the database
+        
+        return jsonify({
+            'error': False,
+            'fileUrl': fileUrl,
+            'message': 'Image Updated successfully.'
+        })
+    else:
+        return jsonify({
+            'error': True,
+            'message': 'Invalid request method.'
         })
