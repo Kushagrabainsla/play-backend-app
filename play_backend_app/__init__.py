9ce8a374a706077967129039b0bd8c90b8470416
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-10-23 17:24:26 +0530

    Added firebase storage for content delivery

--- play_backend_app/__init__.py
+++ play_backend_app/__init__.py
@@ -6,6 +6,25 @@ from pymongo import MongoClient
 from flask_socketio import SocketIO
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from firebase_admin import credentials, initialize_app, storage
+
+credentialJsonDict = {
+    "type": "service_account",
+    "project_id": "play0-1",
+    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY"),
+    "client_email": "play0-1@appspot.gserviceaccount.com",
+    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/play0-1%40appspot.gserviceaccount.com"
+}
+
+# Firebase Bucket
+cred = credentials.Certificate(credentialJsonDict)
+initialize_app(cred, {'storageBucket': 'play0-1.appspot.com'})
+bucket = storage.bucket()
 
 
 # Logger
